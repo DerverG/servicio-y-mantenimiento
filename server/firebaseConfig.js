417835794eaf29a89878a0c0b5cbf0f80809d535
Derver <juan17nov@hotmail.com>
2024-09-07 22:35:18 -0700

    initialize project

---
+++ server/firebaseConfig.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: process.env.API_FIREBASE,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+}
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+
+// Exportar las instancias necesarias
+export { app, analytics }
